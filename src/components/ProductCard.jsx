2bf0dfe7afdb4667af3c3e44e266f405d9aff9d9
zgfpeter <zgfpeter@gmail.com>
2025-09-04 11:48:43 -0700

    Added functionality for fetching and rendering products from an api.

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,7 +1,14 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 
-export default function ProductCard() {
+export default function ProductCard({
+  id,
+  title,
+  price,
+  description,
+  category,
+  image,
+}) {
   return (
     <div className="grid gap-y-2 gap-x-3">
       <img
@@ -11,13 +18,13 @@ export default function ProductCard() {
       />
       <div className="grid">
         <div className="flex place-content-between p-2">
-          <h2 className="">MadeUp Camera </h2>
+          <h2 className="">{title}</h2>
           <div className="flex align-center gap-2 pl-2 pr-2">
             <div className="bg-zinc-500 w-3 h-3 self-center border-1"></div>
             <p>+2</p>
           </div>
         </div>
-        <p className="w-full p-2">€ 125.90</p>
+        <p className="w-full p-2">€ {price}</p>
         <button className="pr-5 justify-self-start pl-5 pt-2 pb-2 text-cyan-700 mb-2 hover:text-teal-600 hover:cursor-pointer">
           <FontAwesomeIcon icon={faCartShopping} />
         </button>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,24 +2,31 @@ import Navbar from "../components/Navbar.jsx";
 import FiltersBar from "../components/FiltersBar.jsx";
 import ProductCard from "../components/ProductCard.jsx";
 import PromoBanner from "../components/PromoBanner.jsx";
+import { useEffect, useState } from "react";
 function Home() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    async function fetchProducts() {
+      try {
+        const response = await fetch("https://fakestoreapi.com/products");
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        console.log("Error white fetching data: ", error);
+      }
+    }
+    fetchProducts();
+  }, []);
+
   return (
     <div className="grid">
       <Navbar />
       <PromoBanner headingLevel={"h2"} />
       <FiltersBar />
       <div className="grid grid-flow-row grid-cols-2 gap-x-1">
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
+        {products.map((product) => (
+          <ProductCard key={product.id} {...product} />
+        ))}
       </div>
     </div>
   );
