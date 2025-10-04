a14f0c98f0851cead8561bd24b00e7243b2e4948
zgfpeter <zgfpeter@gmail.com>
2025-10-04 15:25:44 +0300

    Minor changes to paths

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,9 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
+const basename = "/guava-mini";
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename={basename}>
     <App />
   </BrowserRouter>
 );
