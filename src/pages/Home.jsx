c9eb96122baa3da0a8224ce4a536196502829185
zgfpeter <zgfpeter@gmail.com>
2025-10-04 16:35:09 +0300

    Minor path issues

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -34,7 +34,9 @@ export default function Home() {
   useEffect(() => {
     async function fetchProducts() {
       try {
-        const response = await fetch("products_data.json");
+        const response = await fetch(
+          `${import.meta.env.BASE_URL}products_data.json`
+        );
         if (!response.ok) throw new Error("Network response was not ok"); // Prevents silent failures
 
         // await new Promise((resolve) => setTimeout(resolve, 2000));
