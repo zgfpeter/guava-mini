f92dd9b0f7450b366ff128ee328fc58adb6fa135
zgfpeter <zgfpeter@gmail.com>
2025-10-04 16:27:36 +0300

    Minor responsivity issues on contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -7,10 +7,10 @@ export default function Contact() {
   return (
     <div>
       <Header />
-      <main className="min-h-screen flex flex-col items-center justify-center px-5 md:max-w-[40%] mx-auto">
-        <h1 className="p-5 mb-10 bg-amber-700 text-white font-bold w-full text-center font-[DM_Serif_Display]">
-          CONTACT
-        </h1>
+      <h1 className="p-5 mb-10 bg-amber-700 text-white font-bold w-full text-center font-[DM_Serif_Display]">
+        CONTACT
+      </h1>
+      <main className="w-full max-w-xl justify-self-center">
         <h2 className="p-5 mt-5 text-center text-cyan-900">
           Before contacting us,
           <br /> please make sure to check out our <br />
