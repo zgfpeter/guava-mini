484a2919a3eeba471fbe9e7295cdefab4e247a03
zgfpeter <zgfpeter@gmail.com>
2025-10-06 10:34:16 +0300

    Switched to HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { createRoot } from "react-dom/client";
+import { createRoot, ReactDOM } from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
